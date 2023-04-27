741288c51e4a8b8a5a78f332d33f4b50aafbef3e
ahmet bulutluoz <abbulutluoz@gmail.com>
2023-04-27 11:13:19 +0200

    2.gun 4. commit

--- src/day01/C03.java
+++ src/day01/C03.java
@@ -5,5 +5,12 @@ public class C03 {
         System.out.println("2.gun ilk class");
 
         System.out.println("aciklama");
+        /*
+        Kisisel kullanimda eski commitlere donmek istedigimizde
+        tek bir git komutu yeterli olur
+
+        ancak islemi yapmadan once projemizde commit yaparak
+        son halini git'e gondermeliyiz
+         */
     }
 }
